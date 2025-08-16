949031774a8b410cb004197182551cd7fbbbeb7e
Vasu604 <vanishsharma224@gmail.com>
2025-08-16 12:37:20 +0530

    inc. file size vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import path from 'path';
-import { visualizer } from 'rollup-plugin-visualizer';
-import vitePluginImp from 'vite-plugin-imp';
-
+import path from 'path'
+import { visualizer } from 'rollup-plugin-visualizer'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer(), vitePluginImp({
-    libList: [
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer(),
+    vitePluginImp({
+      libList: [
         {
           libName: 'antd',
           style: (name) => `antd/es/${name}/style`, // loads CSS on demand
         },
       ],
-  })],
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1500, // increase limit (default 500kB)
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          antd: ['antd'],
+        },
+      },
+    },
+  },
 })
-
